ca9ae0f1491e4cf1a7560c9af1a8fb8034d2a132
Leo Voon <voonlihhaur@gmail.com>
2020-02-17 22:17:48 +0800

    Add modal clear todos

--- js/app.js
+++ js/app.js
@@ -1,78 +1,125 @@
 // Register serviceWorker
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
     })
-  }
+}
 
 // Select the Elements
-const clear = document.querySelector(".clear");
-const dateElement = document.getElementById("date");
-const list = document.getElementById("list");
-const input = document.getElementById("input");
-const todoBtn = document.getElementById("todoBtn");
-
+const clear = document.querySelector(".clear")
+const dateElement = document.getElementById("date")
+const list = document.getElementById("list")
+const input = document.getElementById("input")
+const todoBtn = document.getElementById("todoBtn")
+const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const confirmClear = document.querySelector('.confirm')
 // Classes names
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH = "lineThrough";
+const CHECK = "fa-check-circle"
+const UNCHECK = "fa-circle-thin"
+const LINE_THROUGH = "lineThrough"
 
 // Variables
 let LIST, id;
 
 // get item from localstorage
-let data = localStorage.getItem("TODO");
+let data = localStorage.getItem("TODO")
 
 // check if data is not empty
-if(data){
+if (data) {
     LIST = JSON.parse(data);
     id = LIST.length; // set the id to the last one in the list
     loadList(LIST); // load the list to the user interface
-}else{
+} else {
     // if data isn't empty
     LIST = [];
     id = 0;
 }
 
 // load items to the user's interface
-function loadList(array){
-    array.forEach(function(item){
+function loadList(array) {
+    array.forEach(function (item) {
         addToDo(item.name, item.id, item.done, item.trash);
     });
 }
 
 // clear the local storage
-clear.addEventListener("click", function(){
+
+
+// dialog model 
+confirmClear.addEventListener("click", () => {
     localStorage.clear();
     location.reload();
 });
 
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
+
 // Show todays date
-const options = {weekday : "long", month:"short", day:"numeric"};
+const options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric"
+};
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 // add to do function
-function addToDo(toDo, id, done, trash){
-    
-    if(trash){ return; }
-    
+function addToDo(toDo, id, done, trash) {
+
+    if (trash) {
+        return;
+    }
+
     const DONE = done ? CHECK : UNCHECK;
     const LINE = done ? LINE_THROUGH : "";
-    
+
     const item = `<li class="item">
                     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
                     <p class="text ${LINE}">${toDo}</p>
                     <i class="fa fa-minus-circle de" job="delete" id="${id}"></i>
                   </li>
                 `;
-    
+
     const position = "beforeend";
-    
+
     list.insertAdjacentHTML(position, item);
 }
 
@@ -81,24 +128,24 @@ function addToDo(toDo, id, done, trash){
 
 
 // add an item to the list user the enter key
-document.addEventListener("keyup",function(event){
-    if(event.keyCode == 13){
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
         const toDo = input.value;
-        
+
         // if the input isn't empty
-        if(toDo){
+        if (toDo) {
             addToDo(toDo, id, false, false);
-            
+
             LIST.push({
-                name : toDo,
-                id : id,
-                done : false,
-                trash : false
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
             });
-            
+
             // add item to localstorage ( this code must be added where the LIST array is updated)
             localStorage.setItem("TODO", JSON.stringify(LIST));
-            
+
             id++;
         }
         input.value = "";
@@ -108,22 +155,22 @@ document.addEventListener("keyup",function(event){
 
 //add toDo button click
 
-todoBtn.addEventListener('click', function(e){
+todoBtn.addEventListener('click', function (e) {
     const toDo = input.value;
 
-    if(toDo){
+    if (toDo) {
         addToDo(toDo, id, false, false);
-        
+
         LIST.push({
-            name : toDo,
-            id : id,
-            done : false,
-            trash : false
+            name: toDo,
+            id: id,
+            done: false,
+            trash: false
         });
-        
+
         // add item to localstorage ( this code must be added where the LIST array is updated)
         localStorage.setItem("TODO", JSON.stringify(LIST));
-        
+
         id++;
     }
     input.value = "";
@@ -138,51 +185,33 @@ todoBtn.addEventListener('click', function(e){
 
 
 // complete to do
-function completeToDo(element){
+function completeToDo(element) {
     element.classList.toggle(CHECK);
     element.classList.toggle(UNCHECK);
     element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
-    
+
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 // remove to do
-function removeToDo(element){
+function removeToDo(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
-    
+
     LIST[element.id].trash = true;
 }
 
 // target the items created dynamically
 
-list.addEventListener("click", function(event){
+list.addEventListener("click", function (event) {
     const element = event.target; // return the clicked element inside list
     const elementJob = element.attributes.job.value; // complete or delete
-    
-    if(elementJob == "complete"){
+
+    if (elementJob == "complete") {
         completeToDo(element);
-    }else if(elementJob == "delete"){
+    } else if (elementJob == "delete") {
         removeToDo(element);
     }
-    
+
     // add item to localstorage ( this code must be added where the LIST array is updated)
     localStorage.setItem("TODO", JSON.stringify(LIST));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
